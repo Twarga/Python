e1ad204f2189ae6b2f7c2dd8297fd5e2e480958d
Youness Touzani <103898544+Twarga@users.noreply.github.com>
2023-09-15 12:37:31 +0100

    x11

---
+++ train11/x11.py
@@ -0,0 +1,35 @@
+import random 
+import time 
+import math 
+from termcolor import colored , cprint 
+
+
+
+
+
+
+scr = 0 
+x = 0
+start_time = time.time()
+end_time = start_time + 500
+while x <= 10 :
+    while time.time() < end_time:
+        random.seed()
+        rn = random.randint(10,99)
+        S = rn * 11
+        PS = int(input(f"Enter the solution of {rn} * 11 : "))
+        if PS == S :
+            cprint( f"The Answer is correct {rn} * 11 = {S}", "white" , "on_green")
+            scr += 1 
+        else :
+            cprint(f" The Answer is incorrect {rn} * 11 = {S}" , "black" , "on_red")
+    x += 1 
+
+
+
+print(f"Your Score for this game is {scr}")
+
+
+
+
+    
